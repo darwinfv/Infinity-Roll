4111d1ef4ec7c1dc9600c51afa82494b76248765
Darwin Vaz <dvaz@purdue.edu>
2017-05-22 15:04:41 -0400

    Added object spawner

---
+++ Transfer/Object_Spawner.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Object_Spawner : MonoBehaviour {
+
+	public GameObject SpikesPrefab;
+
+	// Use this for initialization
+	void Start () {
+		Instantiate (SpikesPrefab, new Vector3 (0, 0, 5), Quaternion.identity);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
